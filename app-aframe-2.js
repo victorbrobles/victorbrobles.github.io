3370a7d346497476b29e8bb1bdffb6780f9fc6a4
victorbrobles <victorblasco7@hotmail.com>
2021-11-07 13:00:20 +0100

    Actualizacion
    Actualizacion

--- app-aframe-2.js
+++ app-aframe-2.js
@@ -14,172 +14,151 @@ function nums_random (min, max) {
   }
 }
 
+
+let entity = "";
+let plane = "";
+let box = "";
+let position_plane = [];
+
+let limit_x_max = "";
+let limit_x_min = "";
+let limit_y_max = "";
+let limit_y_min = "";
+
+let color_boxes_1 = ["#632f4f", "#8c3c5d", "#c75874", "#ff8b95", "#ffcbb7"];
+let color_boxes_2 = ["#00134c", "#0f2d8b", "#009ffa", "#00fdff", "#cfffff"];
+let color_boxes_3 = ["#03fab7", "#03fd6e", "#c8ff53", "#fffb74", "#fffb9e"];
+let color_boxes_4 = ["#5c2738", "#7d3946", "#ae5a57", "#ea866e", "#f1bd7b"];
+
+let color_planes_1 = ["#0a0a0a", "#5e544f"];
+var color_planes_2 = ["#aba5a3", "#f8f8ff"];
+
+
+let props_planes =["num_plane:0; position:-8 5 -10; width:16; height:8; color:#0a0a0a; colors:" + color_planes_1,
+  "num_plane:1; position:-8 -3 -10; width:16; height:8; color:#aba5a3; colors:" + color_planes_2,
+  "num_plane:2; position:8 5 -10; width:16; height:8; color:#aba5a3; colors:" + color_planes_2,
+  "num_plane:3; position:8 -3 -10; width:16; height:8; color:#0a0a0a; colors:" + color_planes_1];
+
+let props_boxes =["depth:2; width:1; height:1; color:red; colors:" + color_boxes_1,
+  "depth:2; width:1; height:1; color:blue; colors:" + color_boxes_2,
+  "depth:2; width:1; height:1; color:green; colors:" + color_boxes_3,
+  "depth:2; width:1; height:1; color:orange; colors:" + color_boxes_4];
+
+let super_hands = "colliderEvent: raycaster-intersection; colliderEventProperty: els; colliderEndEvent: raycaster-intersection-cleared; colliderEndEventProperty: clearedEls"
+
+
 AFRAME.registerComponent('main_component', {
 
   init: function() {
+
     var scene = document.querySelector("a-scene");
 
-    //Entidad 1
+    entity = document.createElement ("a-entity");
+    entity.setAttribute('cursor', 'rayOrigin:mouse');
+    entity.setAttribute('super-hands', super_hands);
+    entity.id = "entity" + 0;
 
-    var entity1 = document.createElement ("a-entity");
-    //entity1.setAttribute('cursor', 'rayOrigin:mouse');
-    entity1.setAttribute('oculus-touch-controls', 'hand: left');
-    entity1.setAttribute('oculus-touch-controls', 'hand: right');
-    scene.appendChild(entity1);
+    scene.appendChild(entity);
 
-    var plano1 = document.createElement ("a-plane");
-    plano1.id = "plano1";
-    plano1.setAttribute('position', {x:-8, y:5, z:-10});
-    plano1.setAttribute('width', 16);
-    plano1.setAttribute('height', 8);
-    plano1.setAttribute('color', "#0a0a0a");
+    plane = document.createElement ("a-plane");
+    plane.setAttribute('plane-component', props_planes[0]);
+    entity.appendChild(plane);
 
-    var box1 = document.createElement ("a-box");
-    box1.id = "box1";
-    box1.setAttribute('position', {x:-8, y:5, z:-10});
-    box1.setAttribute('color', "red");
+    scene.appendChild(entity);
 
-    entity1.appendChild(plano1);
-    entity1.appendChild(box1);
+    plane = document.createElement ("a-plane");
+    plane.setAttribute('plane-component', props_planes[1]);
+    entity.appendChild(plane);
 
-    //Entidad 2
+    scene.appendChild(entity);
 
-    var entity2 = document.createElement ("a-entity");
-    //entity2.setAttribute('cursor', 'rayOrigin:mouse');
-    entity2.setAttribute('oculus-touch-controls', 'hand: left');
-    entity2.setAttribute('oculus-touch-controls', 'hand: right');
-    scene.appendChild(entity2);
+    plane = document.createElement ("a-plane");
+    plane.setAttribute('plane-component', props_planes[2]);
+    entity.appendChild(plane);
 
-    var plano2 = document.createElement ("a-plane");
-    plano2.id = "plano2";
-    plano2.setAttribute('position', {x:-8, y:-3, z:-10});
-    plano2.setAttribute('width', 16);
-    plano2.setAttribute('height', 8);
-    plano2.setAttribute('color', "#aba5a3");
+    scene.appendChild(entity);
 
-    var box2 = document.createElement ("a-box");
-    box2.id = "box2";
-    box2.setAttribute('position', {x:-8, y:-3, z:-10});
-    box2.setAttribute('color', "blue");
+    plane = document.createElement ("a-plane");
+    plane.setAttribute('plane-component', props_planes[3]);
+    entity.appendChild(plane);
+  }
 
-    entity2.appendChild(plano2);
-    entity2.appendChild(box2);
+});
 
-    //Entidad 3
 
-    var entity3 = document.createElement ("a-entity");
-    //entity3.setAttribute('cursor', 'rayOrigin:mouse');
-    entity3.setAttribute('oculus-touch-controls', 'hand: left');
-    entity3.setAttribute('oculus-touch-controls', 'hand: right');
-    scene.appendChild(entity3);
+AFRAME.registerComponent('plane-component', {
+  schema: {
+    position: {default:"0 0 0"},
+    width: {default:8},
+    height: {default:8},
+    color: {default:"black"},
+    colors: {default:[]},
+    num_plane: {default:0}
+  },
 
-    var plano3 = document.createElement ("a-plane");
-    plano3.id = "plano3";
-    plano3.setAttribute('position', {x:8, y:5, z:-10});
-    plano3.setAttribute('width', 16);
-    plano3.setAttribute('height', 8);
-    plano3.setAttribute('color', "#aba5a3");
+  init: function () {
+    var el = this.el;
+    var data = this.data;
 
-    var box3 = document.createElement ("a-box");
-    box3.id = "box3";
-    box3.setAttribute('position', {x:8, y:5, z:-10});
-    box3.setAttribute('color', "green");
+    el.setAttribute('position', data.position);
+    el.setAttribute('width', data.width);
+    el.setAttribute('height', data.height);
+    el.setAttribute('color', data.color);
 
-    entity3.appendChild(plano3);
-    entity3.appendChild(box3);
-
-    //Entidad 4
-
-    var entity4 = document.createElement ("a-entity");
-    //entity4.setAttribute('cursor', 'rayOrigin:mouse');
-    entity4.setAttribute('oculus-touch-controls', 'hand: left');
-    entity4.setAttribute('oculus-touch-controls', 'hand: right');
-    scene.appendChild(entity4);
-
-    var plano4 = document.createElement ("a-plane");
-    plano4.id = "plano4";
-    plano4.setAttribute('position', {x:8, y:-3, z:-10});
-    plano4.setAttribute('width', 16);
-    plano4.setAttribute('height', 8);
-    plano4.setAttribute('color', "#0a0a0a");
-
-    var box4 = document.createElement ("a-box");
-    box4.id = "box4";
-    box4.setAttribute('position', {x:8, y:-3, z:-10});
-    box4.setAttribute('color', "orange");
-
-    entity4.appendChild(plano4);
-    entity4.appendChild(box4);
-
+    box = document.createElement ("a-box");
+    box.setAttribute('box-component', props_boxes[data.num_plane] + "; position:0 0 0");
+    box.id = "box" + data.num_plane;
+    el.appendChild(box);
   },
 
   update: function() {
+    var el = this.el;
+    var data = this.data;
+    var color_plano = el.getAttribute ("color");
 
-    var colors1 = ["#632f4f", "#8c3c5d", "#c75874", "#ff8b95", "#ffcbb7"];
-    var box1 = document.getElementById("box1");
-
-    //box1.addEventListener('click', function () {
-    box1.addEventListener('abuttondown', function () {
-      box1.setAttribute('color', colors1[Math.floor(Math.random()*colors1.length)]);
+    el.addEventListener('click', function () {
+      while (el.getAttribute ("color") == color_plano) {
+        color_plano = data.colors[Math.floor(Math.random()*data.colors.length)];
+      }
+      el.setAttribute('color', color_plano);
+      box = document.getElementById("box" + data.num_plane);
+      box.setAttribute('position', {x:nums_random(-7,7), y:nums_random(-3,3), z:0});
     });
+  }
+});
 
-    var colors2 = ["#00134c", "#0f2d8b", "#009ffa", "#00fdff", "#cfffff"];
-    var box2 = document.getElementById("box2");
+AFRAME.registerComponent('box-component', {
+  schema: {
+    depth: {default:0},
+    width: {default:8},
+    height: {default:8},
+    color: {default:"black"},
+    colors: {default:[]},
+    position: {default: "0 0 0"}
+  },
 
-    //box2.addEventListener('click', function () {
-    box2.addEventListener('abuttondown', function () {
-      box2.setAttribute('color', colors2[Math.floor(Math.random()*colors2.length)]);
+  init: function () {
+    var el = this.el;
+    var data = this.data;
+
+    el.setAttribute('depth', data.depth);
+    el.setAttribute('width', data.width);
+    el.setAttribute('height', data.height);
+    el.setAttribute('color', data.color);
+    el.setAttribute('colors', data.colors);
+    el.setAttribute('position', data.position);
+  },
+
+  update: function() {
+    var el = this.el;
+    var data = this.data;
+    var color_box = el.getAttribute ("color");
+
+    el.addEventListener('click', function () {
+      while (el.getAttribute ("color") == color_box) {
+        color_box = data.colors[Math.floor(Math.random()*data.colors.length)];
+      }
+      el.setAttribute('color', color_box);
     });
-
-    var colors3 = ["#03fab7", "#03fd6e", "#c8ff53", "#fffb74", "#fffb9e"];
-    var box3 = document.getElementById("box3");
-
-    //box3.addEventListener('click', function () {
-    box3.addEventListener('abuttondown', function () {
-      box3.setAttribute('color', colors3[Math.floor(Math.random()*colors3.length)]);
-    });
-
-    var colors4 = ["#5c2738", "#7d3946", "#ae5a57", "#ea866e", "#f1bd7b"];
-    var box4 = document.getElementById("box4");
-
-    //box4.addEventListener('click', function () {
-    box4.addEventListener('abuttondown', function () {
-      box4.setAttribute('color', colors4[Math.floor(Math.random()*colors4.length)]);
-    });
-
-    var colors5 = ["#0a0a0a", "#5e544f"];
-    var plano1 = document.getElementById("plano1");
-
-    //plano1.addEventListener('click', function () {
-    plano1.addEventListener('abuttondown', function () {
-      plano1.setAttribute('color', colors5[Math.floor(Math.random()*colors5.length)]);
-      box1.setAttribute('position', {x:nums_random(-1,-15), y:nums_random(2,8), z:-10});
-    });
-
-    var colors6 = ["#aba5a3", "#f8f8ff"];
-    var plano2 = document.getElementById("plano2");
-
-    //plano2.addEventListener('click', function () {
-    plano2.addEventListener('abuttondown', function () {
-      plano2.setAttribute('color', colors6[Math.floor(Math.random()*colors6.length)]);
-      box2.setAttribute('position', {x:nums_random(-1,-15), y:nums_random(-6,0), z:-10});
-    });
-
-    var plano3 = document.getElementById("plano3");
-
-    //plano3.addEventListener('click', function () {
-    plano3.addEventListener('abuttondown', function () {
-      plano3.setAttribute('color', colors6[Math.floor(Math.random()*colors6.length)]);
-      box3.setAttribute('position', {x:nums_random(1,15), y:nums_random(2,8), z:-10});
-    });
-
-    var plano4 = document.getElementById("plano4");
-
-    //plano4.addEventListener('click', function () {
-    plano4.addEventListener('abuttondown', function () {
-      plano4.setAttribute('color', colors5[Math.floor(Math.random()*colors5.length)]);
-      box4.setAttribute('position', {x:nums_random(1,15), y:nums_random(-6,0), z:-10});
-    });
-
   }
 });
--- rebotadores.js
+++ rebotadores.js
@@ -4,6 +4,25 @@ let llegaDerecha = [];
 let pararRebotador = [];
 let contador = 0;
 
+let wall1 = "position: -15.5 2 -10; color:black; width:1; height:15";
+let wall2 = "position: 15.5 2 -10; color:black; width:1; height:15";
+let wall3 = "position: 0 9 -10; color:black; width:32; height:1";
+let wall4 = "position: 0 -5 -10; color:black; width:32; height:1";
+
+let walls = [wall1, wall2, wall3, wall4];
+
+let hole1 = "position: -15.5 9 -10; color:#9b9b9b; width:1; height:1";
+let hole2 = "position: -15.5 -5 -10; color:#9b9b9b; width:1; height:1";
+let hole3 = "position: 15.5 9 -10; color:#9b9b9b; width:1; height:1";
+let hole4 = "position: 15.5 -5 -10; color:#9b9b9b; width:1; height:1";
+let hole5 = "position: 0 9 -10; color:#9b9b9b; width:1; height:1";
+let hole6 = "position: 0 -5 -10; color:#9b9b9b; width:1; height:1";
+
+let holes = [hole1, hole2, hole3, hole4, hole5, hole6];
+
+
+
+
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
@@ -13,67 +32,6 @@ function getRandomColor() {
   return color;
 }
 
-function registrarRebotador(contador) {
-  AFRAME.registerComponent('rebotador' + contador, {
-
-    init: function() {
-      llegaArriba[contador] = false;
-      llegaDerecha[contador] = false;
-      pararRebotador[contador] = false;
-    },
-
-    update: function() {
-      var el = this.el;
-      el.addEventListener('click', function() {
-        pararRebotador[contador] = false;
-      });
-    },
-
-    tick: function() {
-      var el = this.el;
-      var positionTmp = this.positionTmp = this.positionTmp || {x: 0, y: 2, z: -10};
-      if (!pararRebotador[contador]) {
-        var position = el.getAttribute("position");
-        if (!llegaArriba[contador] && !llegaDerecha[contador]) {
-          positionTmp.x = position.x + 0.05;
-          positionTmp.y = position.y + 0.05;
-          positionTmp.z = position.z;
-        } else if (llegaArriba[contador] && !llegaDerecha[contador]){
-          positionTmp.x = position.x + 0.05;
-          positionTmp.y = position.y - 0.05;
-          positionTmp.z = position.z;
-        } else if (llegaArriba[contador] && llegaDerecha[contador]) {
-          positionTmp.x = position.x - 0.05;
-          positionTmp.y = position.y - 0.05;
-          positionTmp.z = position.z;
-        } else if (!llegaArriba[contador] && llegaDerecha[contador]) {
-          positionTmp.x = position.x - 0.05;
-          positionTmp.y = position.y + 0.05;
-          positionTmp.z = position.z;
-        }
-
-        el.setAttribute('position', positionTmp);
-
-        if (position.y >= 7) {
-          llegaArriba[contador] = true;
-          el.setAttribute('color', getRandomColor());
-        }
-        if (position.y <= -7) {
-          llegaArriba[contador] = false;
-          el.setAttribute('color', getRandomColor());
-        }
-        if (position.x >= 15.5) {
-          llegaDerecha[contador] = true;
-          el.setAttribute('color', getRandomColor());
-        }
-        if (position.x <= -15.5) {
-          llegaDerecha[contador] = false;
-          el.setAttribute('color', getRandomColor());
-        }
-      }
-    }
-  });
-}
 
 AFRAME.registerComponent('main_component', {
 
@@ -84,50 +42,216 @@ AFRAME.registerComponent('main_component', {
     entity.setAttribute('cursor', 'rayOrigin:mouse');
     scene.appendChild(entity);
 
-    var plano = document.createElement("a-plane");
-    plano.setAttribute('position', {x:0, y:2, z:-10});
-    plano.setAttribute('color', "black");
-    plano.setAttribute('width', 32);
-    plano.setAttribute('height', 15);
+    var plane = document.createElement("a-plane");
+    plane.setAttribute('main-plane', "position: 0 2 -10; color:#35682d; width:32; height:15");
+    entity.appendChild(plane);
 
-    entity.appendChild(plano);
+    for (let i=0; i<walls.length; i++) {
+      var plane = document.createElement("a-plane");
+      plane.setAttribute('wall', walls[i]);
+      entity.appendChild(plane);
+    }
+
+    for (let i=0; i<holes.length; i++) {
+      var plane = document.createElement("a-plane");
+      plane.setAttribute('hole', holes[i]);
+      entity.appendChild(plane);
+    }
 
     var boton = document.createElement("a-box");
-    boton.setAttribute('position', {x:0, y:-6, z:-10});
-    boton.setAttribute('color', "red");
-    boton.setAttribute('width', 3);
-    boton.setAttribute('height', 1);
-    boton.setAttribute('depth', 0);
+    boton.setAttribute('boton',"");
     entity.appendChild(boton);
+  }
+});
 
-    var texto = document.createElement("a-text");
-    texto.setAttribute('value', "Parar rebotadores");
-    texto.setAttribute('align', 'center');
-    boton.appendChild(texto);
+AFRAME.registerComponent('main-plane', {
+  schema: {
+    position: {default: "0 0 0"},
+    color: {default: "black"},
+    width: {default: 0},
+    height: {default: 0}
+  },
+
+  init: function () {
+    var el = this.el;
+    var data = this.data;
+
+    el.setAttribute('position', data.position);
+    el.setAttribute('width', data.width);
+    el.setAttribute('height', data.height);
+    el.setAttribute('color', data.color);
   },
 
   update: function() {
 
-    var plano = document.querySelector("a-plane");
-    plano.addEventListener('click', function () {
+    var el = this.el;
+    var data = this.data;
+
+    el.addEventListener('click', function () {
       var rebotador = document.createElement("a-circle");
-      rebotador.setAttribute('color', "white");
-      rebotador.setAttribute('position', {x:0, y:0, z:0});
-      rebotador.setAttribute('radius', 0.5);
-      rebotador.setAttribute ('rebotador' + contador);
-      registrarRebotador(contador);
-      plano.appendChild(rebotador);
+      var rebotador_props = "color:white; position:0 0 0; radius: 0.5; contador:" + contador;
+      rebotador.setAttribute ('rebotador', rebotador_props);
+      el.appendChild(rebotador);
 
       contador += 1;
 
     });
+  },
 
-    var boton = document.querySelector("a-box");
-    boton.addEventListener('click', function() {
-      var listaRebotadores = document.querySelectorAll("a-circle");
-      for (var k=0; k<listaRebotadores.length; k++) {
-        pararRebotador[k] = true;
+  tick: function() {
+    var listaRebotadores = document.querySelectorAll("a-circle");
+    for (let i=0; i<listaRebotadores.length; i++) {
+      var rebotador = listaRebotadores[i];
+      var position = rebotador.getAttribute("position");
+
+      if (position.y >= 6) {
+        llegaArriba[i] = true;
+        rebotador.setAttribute('color', getRandomColor());
       }
+      if (position.y <= -6) {
+        llegaArriba[i] = false;
+        rebotador.setAttribute('color', getRandomColor());
+      }
+      if (position.x >= 14.5) {
+        llegaDerecha[i] = true;
+        rebotador.setAttribute('color', getRandomColor());
+      }
+      if (position.x <= -14.5) {
+        llegaDerecha[i] = false;
+        rebotador.setAttribute('color', getRandomColor());
+      }
+    }
+  }
+});
+
+AFRAME.registerComponent('wall', {
+  schema: {
+    position: {default: "0 0 0"},
+    color: {default: "red"},
+    width: {default: 0},
+    height: {default: 0}
+  },
+
+  init: function () {
+    var el = this.el;
+    var data = this.data;
+
+    el.setAttribute('position', data.position);
+    el.setAttribute('width', data.width);
+    el.setAttribute('height', data.height);
+    el.setAttribute('color', data.color);
+  }
+});
+
+AFRAME.registerComponent('hole', {
+  schema: {
+    position: {default: "0 0 0"},
+    color: {default: "red"},
+    width: {default: 0},
+    height: {default: 0}
+  },
+
+  init: function () {
+    var el = this.el;
+    var data = this.data;
+
+    el.setAttribute('position', data.position);
+    el.setAttribute('width', data.width);
+    el.setAttribute('height', data.height);
+    el.setAttribute('color', data.color);
+  }
+});
+
+  AFRAME.registerComponent('boton', {
+
+    init: function () {
+      var el = this.el;
+      var data = this.data;
+
+      el.setAttribute('position', {x:0, y:-6, z:-10});
+      el.setAttribute('color', "red");
+      el.setAttribute('width', 3);
+      el.setAttribute('height', 1);
+      el.setAttribute('depth', 0);
+
+      var texto = document.createElement("a-text");
+      texto.setAttribute('value', "Parar rebotadores");
+      texto.setAttribute('align', 'center');
+      el.appendChild(texto);
+    },
+
+    update: function () {
+      var el = this.el;
+      var data = this.data;
+
+      el.addEventListener('click', function() {
+        var listaRebotadores = document.querySelectorAll("a-circle");
+        for (var k=0; k<listaRebotadores.length; k++) {
+          pararRebotador[k] = true;
+        }
+      });
+    }
+});
+
+AFRAME.registerComponent('rebotador', {
+  schema: {
+    position: {default: "0 0 0"},
+    color: {default: "white"},
+    radius: {default: 0.5},
+    contador: {default: 0}
+  },
+
+  init: function () {
+
+    var el = this.el;
+    var data = this.data;
+
+    llegaArriba[data.contador] = false;
+    llegaDerecha[data.contador] = false;
+    pararRebotador[data.contador] = false;
+
+    el.setAttribute('position', data.position);
+    el.setAttribute('color', data.color);
+    el.setAttribute('radius', data.radius);
+  },
+
+  update: function() {
+    var el = this.el;
+    var data = this.data;
+
+    el.addEventListener('click', function() {
+      console.log("rebotador " + data.contador);
+      pararRebotador[data.contador] = false;
     });
   },
+
+  tick: function() {
+
+    var el = this.el;
+    var data = this.data;
+
+    var positionTmp = this.positionTmp = this.positionTmp || {x: 0, y: 2, z: -10};
+    if (!pararRebotador[data.contador]) {
+      var position = el.getAttribute("position");
+      if (!llegaArriba[data.contador] && !llegaDerecha[data.contador]) {
+        positionTmp.x = position.x + 0.05;
+        positionTmp.y = position.y + 0.05;
+        positionTmp.z = position.z;
+      } else if (llegaArriba[data.contador] && !llegaDerecha[data.contador]){
+        positionTmp.x = position.x + 0.05;
+        positionTmp.y = position.y - 0.05;
+        positionTmp.z = position.z;
+      } else if (llegaArriba[data.contador] && llegaDerecha[data.contador]) {
+        positionTmp.x = position.x - 0.05;
+        positionTmp.y = position.y - 0.05;
+        positionTmp.z = position.z;
+      } else if (!llegaArriba[data.contador] && llegaDerecha[data.contador]) {
+        positionTmp.x = position.x - 0.05;
+        positionTmp.y = position.y + 0.05;
+        positionTmp.z = position.z;
+      }
+
+      el.setAttribute('position', positionTmp);
+    }
+  }
 });
