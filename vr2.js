558ace3b3866e2a82869824ee01552e09a24a016
victorbrobles <victorblasco7@hotmail.com>
2021-12-01 19:38:55 +0100

    vr2

---
+++ vr2.js
@@ -0,0 +1,24 @@
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+AFRAME.registerComponent('cubo', {
+
+  update: function () {
+    var el = this.el;
+    var data = this.data;
+
+    el.addEventListener('abuttondown', function () {
+      el.setAttribute ('color', getRandomColor());
+    });
+
+  }
+
+});
