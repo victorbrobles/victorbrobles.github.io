84dcb837312c84be5cbb5a79231876022350b5bb
victorbrobles <47352868+victorbrobles@users.noreply.github.com>
2022-04-13 19:47:08 +0200

    Update grab.js

--- success/grab.js
+++ success/grab.js
@@ -17,8 +17,8 @@ AFRAME.registerComponent('cubo', {
     el.addEventListener('grab-start', function(event) {
 
       console.log("He clickado el cubo");
-      console.log("Componente " + el.toString());
-      console.log("Mano " + event.detail.hand.toString());
+      console.log("Componente " + el.outerHTML);
+      console.log("Mano " + event.detail.hand.outerHTML);
 
       el.setAttribute('color', getRandomColor());
 
