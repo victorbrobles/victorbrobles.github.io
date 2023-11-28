18c97686f6192869fd1c64e5f06f1cd8acf65cd6
victorbrobles <47352868+victorbrobles@users.noreply.github.com>
2023-11-28 18:48:36 +0100

    Memoria

--- obstaculos/js/generales.js
+++ obstaculos/js/generales.js
@@ -1,11 +1,8 @@
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+  var colores = ['blue', 'red', 'yellow', 'green'];
+  var indice = Math.floor(Math.random() * 4);
+  return colores[indice];
 }
 
 function damePropsPieza (suelo) {
